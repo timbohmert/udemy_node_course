6514bae99b6911d9471fa913c2dddbcc4363926b
Tim Bohmert <bohm0050@gmail.com>
2020-12-12 18:28:23 -0600

    add functionality for logger method and mounting routers

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
+// connect to database
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// dev logging middleware (using morgan)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
+    );
+});
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // close server & exit process
+    server.close(() => process.exit(1));
 });
