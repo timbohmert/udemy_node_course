9e5ddc5ba80ebd53146d4ec610e172d946274c03
Tim Bohmert <bohm0050@gmail.com>
2020-12-28 17:18:26 -0600

    create functionality to import or destroy the given bootcamps in bootcamps.json

---
+++ seeder.js
@@ -0,0 +1,55 @@
+// file system module
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// load env vars
+dotenv.config({ path: './config/config.env' });
+
+// load models
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+// read JSON files
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// condition to run one of two functions
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
